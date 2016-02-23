817e3da181c6c1711c80540fc157c19ec064617c
backfrip <backfrip@users.noreply.github.com>
2016-02-23 17:14:57 +0000

    Could be cleaner or something, but whatevs.

---
+++ hw03/animate.js
@@ -0,0 +1,54 @@
+console.log("Animation thing is in the other thing that does the thing. Woo!");
+
+var canvas = document.getElementById('playground');
+var ctx = canvas.getContext('2d');
+ctx.fillStyle = '#909090';
+
+var circleButton = document.getElementById('circle');
+var fastCircleButton = document.getElementById('fast-circle');
+var stopCirclesButton = document.getElementById('stop');
+circleButton.addEventListener('click', animateNormalCircle);
+fastCircleButton.addEventListener('click', animateFastCircle);
+stopCirclesButton.addEventListener('click', stopCircles);
+
+var radius = 1;
+var increment = 1;
+var request = null;
+
+function animateCircle() {
+  if (request != null)
+    stopCircles();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+  ctx.arc(canvas.width/2, canvas.height/2, radius, 0, Math.PI * 2);
+  ctx.closePath();
+  ctx.fill();
+  
+  if (radius <= 0 || radius >= canvas.width/2)
+    increment *= -1;
+  
+  radius += increment;
+  if (radius < 0)
+    radius = 0;
+  
+  request = window.requestAnimationFrame(animateCircle);
+}
+
+function animateNormalCircle() {
+  setIncrement(1);
+  animateCircle();
+}
+
+function animateFastCircle() {
+  setIncrement(3);
+  animateCircle();
+}
+
+function stopCircles() {
+  window.cancelAnimationFrame(request);
+  request = null;
+}
+
+function setIncrement(n) {
+  increment = increment / Math.abs(increment) * n;
+}
