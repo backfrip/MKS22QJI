087fa4266209721a6aaf628faf0f6a207ccafed0
backfrip <backfrip@users.noreply.github.com>
2016-02-23 17:37:49 +0000

    Floating point numbers are harder than you'd think...

--- hw03/animate.js
+++ hw03/animate.js
@@ -7,18 +7,31 @@ ctx.fillStyle = '#909090';
 var circleButton = document.getElementById('circle');
 var fastCircleButton = document.getElementById('fast-circle');
 var stopCirclesButton = document.getElementById('stop');
+var dvdButton = document.getElementById('dvd');
 circleButton.addEventListener('click', animateNormalCircle);
 fastCircleButton.addEventListener('click', animateFastCircle);
-stopCirclesButton.addEventListener('click', stopCircles);
+stopCirclesButton.addEventListener('click', stopStuff);
+dvdButton.addEventListener('click', animateDVD);
+
+var logo = new Image();
+logo.src = './logo_dvd.jpg';
+var ratio = logo.width.toFixed(2) / (canvas.width.toFixed(2) / 10.00);
+logo.width = logo.width * ratio;
+logo.height = logo.height * ratio;
+
 
 var radius = 1;
 var increment = 1;
 var request = null;
 
+function animateDVD() {
+  stopStuff();
+  ctx.drawImage(logo, 0, 0, logo.width, logo.height);
+}
+
 function animateCircle() {
-  if (request != null)
-    stopCircles();
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  stopStuff();
+  
   ctx.beginPath();
   ctx.arc(canvas.width/2, canvas.height/2, radius, 0, Math.PI * 2);
   ctx.closePath();
@@ -44,9 +57,16 @@ function animateFastCircle() {
   animateCircle();
 }
 
-function stopCircles() {
-  window.cancelAnimationFrame(request);
-  request = null;
+function stopStuff() {
+  if (request != null) {
+    window.cancelAnimationFrame(request);
+    request = null;
+  }
+  clear();
+}
+
+function clear() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
 
 function setIncrement(n) {
